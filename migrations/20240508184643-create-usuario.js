c37c4658570a1d4666a06a483b888190a608c942
agomezalejo <agomezalejo99@gmail.com>
2024-05-13 19:28:50 -0300

    ult cambios en login y usuarios

--- migrations/20240508184643-create-usuario.js
+++ migrations/20240508184643-create-usuario.js
@@ -23,14 +23,6 @@ module.exports = {
       },
       contraseña: {
         type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
       }
     });
   },
--- models/index.js
+++ models/index.js
@@ -9,12 +9,22 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
+// let sequelize;
+// if (config.use_env_variable) {
+//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
+// } else {
+//   sequelize = new Sequelize(config.database, config.username, config.password, config);
+// }
+
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+
+if (process.env.NODE_ENV === 'production') {
+  sequelize = new Sequelize(config.DATABASE_URL, config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
+
+
 
 fs
   .readdirSync(__dirname)
